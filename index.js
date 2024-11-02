56280a4356a235b5f00610337243efcc914ddae5
Gregory Murray <gr.j.murray@gmail.com>
2024-11-02 13:40:33 -0700

    kj

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, driverName) {
+   return drivers.filter(driver=>driver.toLowerCase() === driverName.toLowerCase())
+}
+
+function fuzzyMatch(drivers,lettersEntered){
+    return drivers.filter(letters=>letters[0].toLowerCase() === lettersEntered[0].toLowerCase())
+
+}
+
+function matchName(drivers,driverName){
+    return drivers.filter(driver=>driver.name === driverName)
+}
